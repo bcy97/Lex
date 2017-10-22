cdda697f2dc5eec00d232f1afab48eb6d905fa2f
151250002 <151250002@smail.nju.edu.cn>
2017-10-22 21:52:56 +0800

    add the function of reverse the RE from infix to postfix

--- src/NFA/ReHandler.java
+++ src/NFA/ReHandler.java
@@ -7,20 +7,96 @@ import java.util.Stack;
 public class ReHandler {
 
 	public static String reChange(String re) {
-		Stack<Character> stack;
-		
+		Stack<Character> stack = new Stack<>();
+		char[] reNew = new char[re.length()];
+		int index = 0;
+
 		for (int i = 0; i < re.length(); i++) {
-			if (isChar(re.charAt(i))) {
-				queue.add(re.charAt(i));
+			char c = re.charAt(i);
+			if (isChar(c)) {
+				reNew[index] = c;
+				index++;
+				if (i <= re.length() - 1 && (re.charAt(i + 1) == '(' || isChar(re.charAt(i + 1)))) {
+					while (!stack.isEmpty() && 2 <= judge(stack.peek())) {
+						reNew[index] = stack.pop();
+						index++;
+					}
+					stack.push('.');
+				}
+			} else {
+
+				// if is (, push ( into the stack
+				if (c == '(') {
+					stack.push(c);
+				}
+
+				// if is ),pop all the elements until meet (
+				if (c == ')') {
+					while (!stack.isEmpty() && stack.peek() != '(') {
+						reNew[index] = stack.pop();
+						index++;
+					}
+					stack.pop();
+				}
+
+				// if is *, add it to the result string,then add a new . to the
+				// stack
+				if (c == '*') {
+					reNew[index] = c;
+					index++;
+					if (i < re.length() - 1 && (re.charAt(i + 1) == '(' || isChar(re.charAt(i + 1)))) {
+						while (!stack.isEmpty() && 2 <= judge(stack.peek())) {
+							reNew[index] = stack.pop();
+							index++;
+						}
+						stack.push('.');
+					}
+				}
+
+				if (c == '|') {
+					while (!stack.isEmpty() && judge(c) <= judge(stack.peek())) {
+						reNew[index] = stack.pop();
+						index++;
+					}
+					stack.push(c);
+				}
 			}
+
 		}
+
+		while (!stack.isEmpty()) {
+			reNew[index] = stack.pop();
+			index++;
+		}
+
+		String result = String.copyValueOf(reNew);
+
+		return result;
 	}
-	
+
 	public static boolean isChar(char a) {
-		if (a>='a'&&a<='z'||a>='A'&&a<='Z') {
+		if (a >= 'a' && a <= 'z' || a >= 'A' && a <= 'Z') {
 			return true;
 		}
 		return false;
 	}
-	
+
+	public static int judge(char c) {
+		switch (c) {
+		case '(':
+			return 0;
+		case '|':
+			return 1;
+		case '.':
+			return 2;
+		}
+		return 0;
+	}
+
+	public static void main(String args[]) {
+		String re = "ab*(a|b)*";
+		System.out.println(reChange(re));
+
+	}
+
 }
